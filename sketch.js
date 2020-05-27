3f27c06bd2aef2a170703e5626414b34555cc589
unknown <apri0372@gmail.com>
2020-05-27 17:41:37 -0400

    after test

--- sketch.js
+++ sketch.js
@@ -50,7 +50,7 @@ function get_joke_of_the_day() {
             txt = xmlhttp.responseText;
           }
         };
-        xmlhttp.open("GET","https://github.com/vasco0372/g_j_bot",true);
+        xmlhttp.open("GET","http://127.0.0.1:8887",true);
         xmlhttp.setRequestHeader("Content-type", "application/json");
         xmlhttp.send(); 
         var joketxt = JSON.parse(txt);
